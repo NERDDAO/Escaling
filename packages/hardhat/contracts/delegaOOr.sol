7d645c1365c4b003a343357d36d7bf14bdb2f48b
Ataxia123 <vp.ca94@gmail.com>
2023-03-13 16:45:22 -0500

    contract changes

--- packages/hardhat/contracts/delegaOOr.sol
+++ packages/hardhat/contracts/delegaOOr.sol
@@ -8,6 +8,7 @@ contract DelegaOOr {
     event DelegationCreated(address indexed delegation);
 
     struct Delegation {
+        string name;
         address delegator;
         address funder;
         address[] signers;
@@ -18,18 +19,23 @@ contract DelegaOOr {
 
     constructor() {}
 
-    function createDelegation(address funder, address[] memory signers, uint signaturesRequired) public returns (address) {
-        DelegatedMultiSig delegation = new DelegatedMultiSig(0, signers, signaturesRequired);
-        delegation.setFunder(funder);
-        emit DelegationCreated(address(delegation));
-        delegations[funder].push(Delegation(msg.sender, funder, signers, signaturesRequired));
-        for (uint i = 0; i < signers.length; i++) {
-            delegations[signers[i]].push(Delegation(msg.sender, funder, signers, signaturesRequired));
-        }
-        return address(delegation);
+function createDelegation(string memory name, address funder, address[] memory signers, uint signaturesRequired) public returns (address) {
+    DelegatedMultiSig delegation = new DelegatedMultiSig(0, signers, signaturesRequired, name);
+    emit DelegationCreated(address(delegation));
+    delegations[funder].push(Delegation(name, msg.sender, funder, signers, signaturesRequired));
+    for (uint i = 0; i < signers.length; i++) {
+        delegations[signers[i]].push(Delegation(name, msg.sender, funder, signers, signaturesRequired));
     }
+    return address(delegation);
+}
 
-    function getDelegationsForWallet(address wallet) public view returns (Delegation[] memory) {
-        return delegations[wallet];
+function getDelegationsForWallet(address wallet) public view returns (string[] memory, Delegation[] memory) {
+    Delegation[] memory walletDelegations = delegations[wallet];
+    string[] memory delegationNames = new string[](walletDelegations.length);
+    for (uint i = 0; i < walletDelegations.length; i++) {
+        delegationNames[i] = walletDelegations[i].name;
     }
+    return (delegationNames, walletDelegations);
+}
+
 }
