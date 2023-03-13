632b7623a9dfedb0f89af69f0379233050b425b3
0xgeeb <0xgeeb@gmail.com>
2023-03-13 18:58:48 -0400

    setup in foundry

---
+++ packages/foundry/src/delegat00r.sol
@@ -0,0 +1,39 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+
+import "./DelegatedMultiSig.sol";
+
+contract DelegaOOr {
+
+  event DelegationCreated(address indexed delegation);
+
+  struct Delegation {
+    string name;
+    address delegator;
+    address funder;
+    address[] signers;
+    uint signaturesRequired;
+  }
+
+  mapping(address => Delegation[]) public delegations;
+
+  function createDelegation(string memory name, address funder, address[] memory signers, uint signaturesRequired) public returns (address) {
+    DelegatedMultiSig delegation = new DelegatedMultiSig(0, signers, signaturesRequired, name);
+    emit DelegationCreated(address(delegation));
+    delegations[funder].push(Delegation(name, msg.sender, funder, signers, signaturesRequired));
+    for (uint i = 0; i < signers.length; i++) {
+      delegations[signers[i]].push(Delegation(name, msg.sender, funder, signers, signaturesRequired));
+    }
+    return address(delegation);
+  }
+
+  function getDelegationsForWallet(address wallet) public view returns (string[] memory, Delegation[] memory) {
+    Delegation[] memory walletDelegations = delegations[wallet];
+    string[] memory delegationNames = new string[](walletDelegations.length);
+    for (uint i = 0; i < walletDelegations.length; i++) {
+      delegationNames[i] = walletDelegations[i].name;
+    }
+    return (delegationNames, walletDelegations);
+  }
+
+}
